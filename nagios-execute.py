e976e80276aaac088b5a8553dcb1cc0332650363
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2013-06-26 19:10:50 +0530

    Add colored output

--- nagios-execute.py
+++ nagios-execute.py
@@ -26,6 +26,25 @@ import os
 import sys
 from multiprocessing import Pool
 from config import config
+import signal
+
+#Some helper functions
+def _wrap_with(code):
+
+    def inner(text, bold=False):
+        c = code
+        if bold:
+            c = "1;%s" % c 
+        return "\033[%sm%s\033[0m" % (c, text)
+    return inner
+
+red = _wrap_with('31')
+green = _wrap_with('32')
+yellow = _wrap_with('33')
+blue = _wrap_with('34')
+magenta = _wrap_with('35')
+cyan = _wrap_with('36')
+white = _wrap_with('37')
 
 paramiko.util.log_to_file('ssh.log')
 
@@ -53,7 +72,7 @@ def execute_on_host((hostname, command), username=config['username'], keyfile=co
         ret_value = stdout.channel.recv_exit_status()
     except:
         ret_value = "unknown"
-    print("%s:[%s] \n%s\n" % (hostname, ret_value, cmdOutput[:-1]))
+    print("%s:[%s] \n%s\n" % (cyan(hostname), yellow(ret_value), white(cmdOutput[:-1])))
     ssh_client.close()
     return (hostname, cmdOutput)
 
